df26dc9328bbada7e7af3fdf3fd0d4b0932df4be
Penjiboy <chavulapenjani@gmail.com>
2018-02-25 10:59:26 -0800

    Updated the header file

--- include/rpm.h
+++ include/rpm.h
@@ -5,13 +5,19 @@
 #ifndef RPM_H
 #define RPM_H
 
+#include <vector>
+
 class Rpm {
 public:
+
+    //The desired RPM, which is set by the system
+    long desiredRPM;
+
     /**
-     * Calculate the current rpm
+     * Calculate the actual rpm
      * @return rpm value
      */
-    long currentRPM(); //calculate the current rpm from the sensor data
+    long actualRPM();
 
     /**
      * checks whether the rpm is decreasing based on the data. Return true if it is decreasing, false otherwise.
@@ -28,6 +34,20 @@ public:
 private:
     //File containing the sensor input data
     File file;
+
+    struct reading {
+        long value; //the actual rpm reading
+        long time;  //the timestamp for the reading
+    };
+
+    // A data structure to hold a number of rpm readings for analysis
+    vector<reading> readings;
+
+    /**
+     * Read the input file to process new rpm readings
+     * @modifies readings vector
+     */
+    void updateReadings();
 };
 
 #endif //RPM_H
