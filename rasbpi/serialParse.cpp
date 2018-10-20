f3da1d785523acf6165128e7d260b6779884c8d6
Alex Lee <AlexLee@server.ca>
2018-10-20 03:48:18 -0700

    implemented serialParse, still need to be tested

--- rasbpi/serialParse.cpp
+++ rasbpi/serialParse.cpp
@@ -1,53 +1,253 @@
 #include "serialParse.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stdlib.h>
 
-std::string SerialParse::setRpm(int rpmVal){
+using namespace std;
+
+
+void SerialParse::setRpm(unsigned int rpmVal){
+    std::string message = "$SET07";
+    message = message + rpmVal + "*";
+    sendMessage(message);
+}
+
+
+
+int SerialParse::getRpm(){
+    std::string message = "$GET07*";
+    
+    std::string returnArg;
+    
+    sendMessage(message);
+    
+    //keep looping until a response is retrieved from the arduino
+    while(!receiveMessage(returnArg)){
+        
+    }
+    
+    //check the ID
+    if(returnArg.subStr(4, 5).compare("07")){
+        return stoi(returnArg.subStr(6, 7));
+    }
+    
+    return -1;
+}
+
+
+
+double SerialParse::getLatitude(){
+    std::string message = "$GET01*";
+    
+    std::string returnArg;
+    
+    sendMessage(message);
+    
+    //keep looping until a response is retrieved from the arduino
+    while(!receiveMessage(returnArg)){
+        
+    }
+    
+    //check the ID
+    if(returnArg.subStr(4, 5).compare("01")){
+        return atof(returnArg.subStr(6, 7).c_str());
+    }
+    
+    return -1;
+}
+
+
+
+void SerialParse::setLatitude(signed double latitudeVal){
+    std::string message = "$SET01";
+    message = message + latitudeVal + "*";
+    sendMessage(message);
     
 }
 
-std::string SerialParse::getLatitude(){
+
+
+double SerialParse::getLongitude(){
+    std::string message = "$GET02*";
     
+    std::string returnArg;
+    
+    sendMessage(message);
+    
+    //keep looping until a response is retrieved from the arduino
+    while(!receiveMessage(returnArg)){
+        
+    }
+    
+    //check the ID
+    if(returnArg.subStr(4, 5).compare("02")){
+        return atof(returnArg.subStr(6, 7).c_str());
+    }
+    
+    return -1;
 }
 
-std::string SerialParse::setLatitude(){
-    
+
+
+void SerialParse::setLongitude(signed double longitudeVal){
+    std::string message = "$SET02";
+    message = message + longitudeVal + "*";
+    sendMessage(message);
 }
 
-std::string SerialParse::getLongitude(){
+
+
+double SerialParse::getMotorPower(){
+    std::string message = "$GET03*";
     
+    std::string returnArg;
+    
+    sendMessage(message);
+    
+    //keep looping until a response is retrieved from the arduino
+    while(!receiveMessage(returnArg)){
+        
+    }
+    
+    //check the ID
+    if(returnArg.subStr(4, 5).compare("03")){
+        return atof(returnArg.subStr(6, 7).c_str());
+    }
+    
+    return -1;
 }
 
-std::string SerialParse::setLongitude(){
-    
+
+
+void SerialParse::setMotorPower(unsigned double watt){
+    std::string message = "$SET03";
+    message = message + watt + "*";
+    sendMessage(message);
 }
 
-std::string SerialParse::getMotorPower(){
+
+double SerialParse::getBatterLevel(){
+    std::string message = "$GET04*";
     
+    std::string returnArg;
+    
+    sendMessage(message);
+    
+    //keep looping until a response is retrieved from the arduino
+    while(!receiveMessage(returnArg)){
+        
+    }
+    
+    //check the ID
+    if(returnArg.subStr(4, 5).compare("04")){
+        return atof(returnArg.subStr(6, 7).c_str());
+    }
+    
+    return -1;
 }
 
-std::string SerialParse::getBatterLevel(){
+
+
+double SerialParse::getMotorTemperature(){
+    std::string message = "$GET05*";
     
+    std::string returnArg;
+    
+    sendMessage(message);
+    
+    //keep looping until a response is retrieved from the arduino
+    while(!receiveMessage(returnArg)){
+        
+    }
+    
+    //check the ID
+    if(returnArg.subStr(4, 5).compare("05")){
+        return atof(returnArg.subStr(6, 7).c_str());
+    }
+    
+    return -1;
 }
 
-std::string SerialParse::getMotorTemperature(){
+
+
+double SerialParse::getBatteryVoltage(){
+    std::string message = "$GET06*";
     
+    std::string returnArg;
+    
+    sendMessage(message);
+    
+    //keep looping until a response is retrieved from the arduino
+    while(!receiveMessage(returnArg)){
+        
+    }
+    
+    //check the ID
+    if(returnArg.subStr(4, 5).compare("06")){
+        return atof(returnArg.subStr(6, 7).c_str());
+    }
+    
+    return -1;
 }
 
-std::string SerialParse::getBatteryVoltage(){
+
+
+double SerialParse::getRudderAngle(){
+    std::string message = "$GET51*";
     
+    std::string returnArg;
+    
+    sendMessage(message);
+    
+    //keep looping until a response is retrieved from the arduino
+    while(!receiveMessage(returnArg)){
+        
+    }
+    
+    //check the ID
+    if(returnArg.subStr(4, 5).compare("51")){
+        return atof(returnArg.subStr(6, 7).c_str());
+    }
+    
+    return -1;
 }
 
-std::string SerialParse::getRudderAngle(){
-    
+
+
+void SerialParse::setRudderAngle(signed double angle){
+    std::string message = "$SET51";
+    message = message + angle + "*";
+    sendMessage(message);
 }
 
-std::string SerialParse::setRudderAngle(){
+
+
+void SerialParse::sendMessage(std::string & message){
+    fstream writeFile;
+    //open the file and set initial position at the end of the file
+    writeFile.open("serialLogs.txt", ios::ate);
+    message = message + "\n";
     
+    writeFile.write( message, sizeof(char)*message.length());
+    
+    writeFile.close();
 }
 
-void SerialParse::sendMessage(){
-    
-}
 
-std::string SerialParse::receiveMessage(){
+
+bool SerialParse::receiveMessage(std::string returnArg){
+    std::ifstream file("serialLogs.txt");
+    std::string str, subString;
     
+    std::getline(file, str);
+    
+    subString = str.subStr(1, 3);
+    
+    if(subString.compare("VAL")){
+        returnArg = str.subStr(0, 7);
+        return true;
+    }
+        
+    return false;
 }
--- rasbpi/serialParse.h
+++ rasbpi/serialParse.h
@@ -12,104 +12,96 @@ public:
     
     /* setRmp
      * parameter: rmpVal - the rpm value to set to
-     * returns: the message that can be used set rpm for the motor
      * ID: 07
      */
-    std::string setRpm(unsigned int rpmVal);
+    void setRpm(unsigned int rpmVal);
     
     /* getRmp
-     * returns: the message that can be used set rpm for the motor
+     * returns: the rpm for the motor
      * ID: 07
      */
-    std::string getRpm();
+    unsigned int getRpm();
     
     /* getLatitude
-     * returns: the message that can be used to getLatitude
+     * returns: the Latitude
      * ID: 01
      */
-    std::string getLatitude();
+    double getLatitude();
     
     /* setLatitude
      * parameter: latitudeVal - the latitude to set to, format: DDDMM.mmmmm
-     * returns: the message that can be used to set latitude
      * ID: 01
      */
-    std::string setLatitude(signed double latitudeVal);
+    void setLatitude(signed double latitudeVal);
     
     /* getLongitude
      * returns: the message that can be used to get longitude
      * ID: 02
      */
-    std::string getLongitude();
+    double getLongitude();
     
     /* setLongitude
      * parameter: longitudeVal - the longitude to set to, format: DDDMM.mmmmm
-     * returns: the message that can be used to set longitude
      * ID: 02
      */
-    std::string setLongitude(signed double longitudeVal);
+    void setLongitude(signed double longitudeVal);
     
     /* getMotorPower
      * returns: the message that can be used to get the motor power
      * ID: 03
      */
-    std::string getMotorPower();
+    double getMotorPower();
     
     /* setMotorPower
-     * returns: the message that can be used to set the motor power, unsigned double
      * ID: 03
      */
-    std::string setMotorPower(unsigned double watt);
+    void setMotorPower(unsigned double watt);
     
     /* getBatteryLevel
-     * returns: the message that can be used to get the battery level
+     * returns: the battery level
      * ID: 04
      */
-    std::string getBatteryLevel();
-    
-    /* setBatteryLevel
-     * parameter: level - the battery level to set to, unsigned double, should b between 1-0
-     * returns: the message that can be used to set the battery level
-     * ID: 04
-     */
-    std::string setBatteryLevel(unsigned double level);
+    double getBatteryLevel();
     
     /* getMotorTemperature
-     * returns: the message that can be used to get the motor temperature, value in kelvin
+     * returns: the motor temperature, value in kelvin
      * ID: 05
      */
-    std::string getMotorTemperature();
+    double getMotorTemperature();
     
     /* getBatteryVoltage
-     * returns: the message that can be used to get the battery level, value is signed double
+     * returns: the battery level, value is signed double
      * ID: 06
      */
-    std::string getbatteryVoltage();
+    double getbatteryVoltage();
     
     /* getRudderAngle
-     * returns: the message that can be used to get the rudder angle, value is signed double
+     * returns: the rudder angle, value is signed double
      * ID: 51
      */
-    std::string getRudderAngle();
+    double getRudderAngle();
     
     /* setRudderAngle
      * parameter: angle - the angle value to set to in signed double
-     * returns: the message that can be used to set tte rudder angle
      * ID: 51
      */
-    std::string setRudderAngle(signed double angle);
+    void setRudderAngle(signed double angle);
     
 private:
     
     /*
-     *
+     * write the message to the file
      */
     void sendMessage(std::string & message);
+
     
     /*
-     *
+     * Read from file to determine if valid arduino response
+     * valid: if start with $VAL
+     * return: true if value false otherwise
+     * returnArg: the argument extracted from the line of code
      */
-    std::string receiveMessage();
+    bool receiveMessage(std::string returnArg);
 };
 
 #endif
