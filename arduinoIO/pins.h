15bbc6fd2ef8ec670955f271cdbdf41dcd66319b
Penjani Chavula <chavulapenjani@gmail.com>
2018-12-02 17:01:57 -0800

    updated arduino motor code

--- arduinoIO/pins.h
+++ arduinoIO/pins.h
@@ -1,7 +1,7 @@
 #ifndef PINS_H
 #define PINS_H
 
-#define MOTOR_ESC_PIN 21 //pin for signal to primary drive motor esc
+#define MOTOR_ESC_PIN 20 //pin for signal to primary drive motor esc
 #define RUDDER_SERVO_PIN 10 //pin for signal to primary rudder servo
 
 #endif
