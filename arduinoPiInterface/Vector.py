05d174c6070cfb506a8068e802697793bba940ac
YYen Gallup <yyengallup@gmail.com>
2018-12-05 04:21:29 -0800

    d

--- arduinoPiInterface/Vector.py
+++ arduinoPiInterface/Vector.py
@@ -8,6 +8,8 @@ class Vector:
     y = 0
 
     def set_from_polar(self, r, rho):
+        rho = float(rho)
+        r = float(r)
         self.x = r * math.cos(math.radians(rho))
         self.y = r * math.sin(math.radians(rho))
 
@@ -19,15 +21,15 @@ class Vector:
         return math.atan2(other.x, other.y) - math.atan2(self.x, self.y)
 
     def set_coords(self, coords):
-        self.x = coords[0]
-        self.y = coords[1]
+        self.x = float(coords[0])
+        self.y = float(coords[1])
 
     def get_coords(self):
         return (self.x, self.y)
 
     def __init__(self, x=0, y=0):
-        self.x = x
-        self.y = y
+        self.x = float(x)
+        self.y = float(y)
 
     def __add__(self, other):
         return Vector(self.x + other.x, self.y + other.y)
