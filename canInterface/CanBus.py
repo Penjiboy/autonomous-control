37853ee275a3bfcc0939cc2038bc283e6634c437
Penjani Chavula <chavulapenjani@gmail.com>
2019-02-04 21:34:46 -0800

    basic setup/config for canBus

--- canInterface/CanBus.py
+++ canInterface/CanBus.py
@@ -1,13 +1,18 @@
 import can
+import os
 
 class CanBus: 
     
-    __bus
+    _bus
 
     def __init__(self):
+        # Initialize the CAN interface
+        os.system('sudo /sbin/ip link set can0 up type can bitrate 500000')
+
         # Gotta check that these config settings actually work for our setup
-        _interface = 'pcan' ### Still need to figure out the most appropriate can interface
-        _channel = 'vcan0' 
+        _interface = 'socketcan_native' ### Still need to figure out the most appropriate can interface
+        _channel = 'can0' 
         _bitrate = 500000
 
-        __bus = can.interface.Bus(bustype=_interface, channel=_channel, bitrate=_bitrate)
+        global _bus
+        _bus = can.interface.Bus(bustype=_interface, channel=_channel)
