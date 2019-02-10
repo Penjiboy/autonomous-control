fe675053be76a7784ea6a05104bbdbe40fd65924
Penjani Chavula <chavulapenjani@gmail.com>
2019-02-09 17:52:33 -0800

    more work done on initializing the CanBus

--- canInterface/CanBus.py
+++ canInterface/CanBus.py
@@ -1,18 +1,39 @@
 import can
 import os
+import datetime
+import time
 
 class CanBus: 
     
     _bus
+    _mainListener
+    _notifier
 
     def __init__(self):
         # Initialize the CAN interface
         os.system('sudo /sbin/ip link set can0 up type can bitrate 500000')
 
-        # Gotta check that these config settings actually work for our setup
-        _interface = 'socketcan_native' ### Still need to figure out the most appropriate can interface
+        _interface = 'socketcan_native'
         _channel = 'can0' 
         _bitrate = 500000
 
+        # Setup the initialize the bus
         global _bus
         _bus = can.interface.Bus(bustype=_interface, channel=_channel)
+
+        # initialize the main listener
+        global _mainListener
+        _mainListener = can.BufferedReader()
+
+        # initialize the file logger
+        ## universal time in UTC
+        ts=time.time()
+        currentTime = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d_%H:%M:%S')
+        fileLogger = can.Printer("logs_" + currentTime + ".txt")
+
+        listenersList = [_mainListener, fileLogger]
+
+        # initialize the notifier
+        global _notifier
+        _notifier = can.Notifier(_bus, listenersList)
+        
