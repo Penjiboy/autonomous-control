b73315b5459631ccf743f73bc17b0239b15e87b0
Penjani Chavula <chavulapenjani@gmail.com>
2019-03-03 22:05:18 -0800

    added some bms code to test and refactored gps interface code

---
+++ canInterface/BmsCanInterface.py
@@ -0,0 +1,40 @@
+import can
+from can import Message
+from canInterface.CanBus import CanBus
+
+class BmsCanInterface(can.Listener):
+    """
+    A BmsCanInterface is a subclass of Listener which handles
+    a message whenever a message is received. Particularly, it will
+    handle messages it identifies as coming from the BMS
+    """
+
+    # don't know yet if we need to specify an id for each type of message, or just the device itself
+    _arbitration_ids = {
+        'battery_pack_voltage': None,
+    }
+
+    def __init__(self, bus):
+        # not much to do here
+        self.bus = bus
+
+    def on_message_received(self, msg):
+        # Parse Message
+        print("BMS interface message received: " + msg.__str__())
+
+        if msg.arbitration_id == self._arbitration_ids['battery_pack_voltage']:
+            self.read_battery_pack_voltage(msg)
+        else : 
+            print("No appropriate bms message handler found for message: " + msg.__str__())
+
+    def read_battery_pack_voltage(self, msg):
+        voltage_bytes = msg.data[2:]
+        voltage = int.from_bytes(voltage_bytes, byteorder='little', signed=True)
+        print("bms battery voltage = " + voltage)
+        self.bus.bms_battery_voltage = voltage
+
+    def send_request(self, arbitration_id, data):
+        message = Message(arbitration_id=arbitration_id, is_extended_id=False, data=data)
+        self.bus.send(message)
+
+        
--- canInterface/CanBus.py
+++ canInterface/CanBus.py
@@ -4,27 +4,24 @@ import datetime
 import time
 import threading
 from can import Message
+from canInterface.GpsCanInterface import GpsCanInterface
+from canInterface.BmsCanInterface import BmsCanInterface
 
 class CanBus: 
     
     _bus = None
-    _mainListener = None
+    _main_listener = None
+    _gps_interface = None
+    _bms_interface = None
     _notifier = None
 
     # global variables
     bms_soc = None
     bms_temps = None
+    bms_battery_voltage = None
     gps_position_detailed = None # GNSSPositionData
     gps_position_abbreviated = None # GNSSPositionRapidUpdates
     gps_speed_and_direction = None # NMEACogSogData
-
-    # don't know yet if we need to specify an id for each type of message, or just the device itself
-    arbitration_ids = {
-        'gps_position_detailed': None,
-        'gps_position_abbreviated': None,
-        'gps_speed_and_direction': None,
-        'bms': None,
-    }
 
     _error_buffer = []
 
@@ -41,9 +38,9 @@ class CanBus:
         _bus = can.interface.Bus(bustype=_interface, channel=_channel)
 
         # initialize the main listener
-        global _mainListener
-        #_mainListener = can.BufferedReader()
-        _mainListener = MessageListener(self)
+        self._main_listener = can.BufferedReader()
+        self._gps_interface = GpsCanInterface(self)
+        self._bms_interface = BmsCanInterface(self)
 
         # initialize the file logger
         ## universal time in UTC
@@ -51,11 +48,10 @@ class CanBus:
         currentTime = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d_%H:%M:%S')
         fileLogger = can.Printer("logs_" + currentTime + ".txt")
 
-        listenersList = [_mainListener, fileLogger]
+        listenersList = [self._main_listener, fileLogger, self._gps_interface, self._bms_interface]
 
         # initialize the notifier
-        global _notifier
-        _notifier = can.Notifier(_bus, listenersList)
+        self._notifier = can.Notifier(_bus, listenersList)
 
         
     def get_new_errors(self):
@@ -65,61 +61,4 @@ class CanBus:
 
     def send_message(self, arbitration_id=0, data=None, is_extended_id=False):
         message = Message(arbitration_id=arbitration_id,is_extended_id=is_extended_id,data=data)
-
-        global _bus
-        _bus.send(message)
-
-
-class MessageListener(can.Listener):
-    """
-    A MessageListener is a subclass of Listener which handles
-    a message whenever a message is received
-    """
-
-    def __init__(self, bus):
-        # not much to do here
-        self.bus = bus
-        pass
-
-    def on_message_received(self, msg):
-        # Parse Message
-        print("Message received: " + msg.__str__())
-        
-        if msg.arbitration_id == self.bus.arbitration_ids['gps_position_detailed']:
-            self.process_gps_position_detailed(msg)
-        elif msg.arbitration_id == self.bus.arbitration_ids['gps_position_abbreviated']:
-            self.process_gps_position_abbreviated(msg)
-        elif msg.arbitration_id == self.bus.arbitration_ids['gps_speed_and_direction']:
-            self.process_gps_speed_and_direction(msg)
-        else :
-            print("No appropriate message handler found for message: " + msg.__str__())
-        
-
-    def process_gps_position_detailed(self,msg):
-        print("processing gps position detailed")
-        pass
-
-    def process_gps_position_abbreviated(self,msg):
-        print("processing gps position abbreviated")
-        result = {}
-
-        # Parse data
-        # latitude is the first 32 bits
-        latitude = msg.data[:5]
-        # convert to a number
-        # multiply by 0.0000001
-        # check that value is between -90 and 90
-
-        # longitude is the last 32 bits
-        longitude = msg.data[5:]
-        # convert to a number
-        # multiply by 0.0000001
-        # check that value is between -180 and 180
-
-        result.latitude = latitude
-        result.longitude = longitude
-        self.bus.gps_position_abbreviated = result
-
-    def process_gps_speed_and_direction(self,msg):
-        print("processing gps speed and direction")
-        pass
+        self._bus.send(message)
