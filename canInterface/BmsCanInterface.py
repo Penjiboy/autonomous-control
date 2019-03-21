b5d8aca6099c6486d95567fc360693506eb2ade5
Penjani Chavula <chavulapenjani@gmail.com>
2019-03-20 18:42:54 -0700

    added some helper methods to the bms interface

--- canInterface/BmsCanInterface.py
+++ canInterface/BmsCanInterface.py
@@ -12,6 +12,24 @@ class BmsCanInterface(can.Listener):
     # don't know yet if we need to specify an id for each type of message, or just the device itself
     _arbitration_ids = {
         'battery_pack_voltage': None,
+        'request_identifier': '01000000001',
+        'respone_identifier': '01001000001'
+    }
+
+    request_unique_ids = {
+        'newest_events': '\x11',
+        'all_events': '\x12',
+        'battery_pack_voltage': '\x14',
+        'battery_pack_current': '\x15',
+        'battery_pack_max_cell_voltage': '\x16',
+        'battery_pack_min_cell_voltage': '\x17',
+        'online_status': '\x18',
+        'lifetime_counter': '\x19',
+        'estimated_SOC_value': '\x1a',
+        'device_temperatures': '\x1b',
+        'battery_pack_cells_voltages': '\x1c',
+        'version': '\x1e',
+        'calculated_speed__left_distance__estimated_time': '\x20',
     }
 
     def __init__(self, bus):
@@ -22,19 +40,43 @@ class BmsCanInterface(can.Listener):
         # Parse Message
         print("BMS interface message received: " + msg.__str__())
 
-        if msg.arbitration_id == self._arbitration_ids['battery_pack_voltage']:
-            self.read_battery_pack_voltage(msg)
+        if msg.arbitration_id == self._arbitration_ids['response_identifier']:
+            self.parse_response(msg.data)
         else : 
             print("No appropriate bms message handler found for message: " + msg.__str__())
 
-    def read_battery_pack_voltage(self, msg):
-        voltage_bytes = msg.data[2:]
-        voltage = int.from_bytes(voltage_bytes, byteorder='little', signed=True)
-        print("bms battery voltage = " + voltage)
-        self.bus.bms_battery_voltage = voltage
+    def parse_response(self, data):
+        if data[1] == self.request_unique_ids['newest_events']:
+            self.read_newest_events(data)
 
-    def send_request(self, arbitration_id, data):
+
+    def read_newest_events(self, data):
+        if data[0] == '\x00':
+            print("error reading newest events")
+            return
+        
+        print("read newest events: " + data.__str__())
+
+    def read_battery_pack_voltage(self, data):
+        if data[0] == '\x00':
+            print("error reading battery pack voltage")
+            return
+
+        voltage = int.from_bytes(data[2:], byteorder='big', signed=True)
+        self.bus.bms_battery_voltage = voltage
+        print("battery pack voltage = " + voltage)
+        return data[2:]
+
+    def send_request(self, data, arbitration_id=_arbitration_ids['request_identifier']):
         message = Message(arbitration_id=arbitration_id, is_extended_id=False, data=data)
         self.bus.send(message)
+
+    def send_generic_request(self, unique_id):
+        request_data = self.generic_request_data(unique_id)
+        message = Message(arbitration_id=self._arbitration_ids['request_identifier'], is_extended_id=False, data=request_data)
+        self.bus.send(message)
+
+    def generic_request_data(self, unique_id):
+        return bytearray(unique_id + '\x00\x00\x00\x00\x00\x00\x00')
 
         
